9a0cfdd2b6c1531b011ea7b921226f7fe5d985b5
rayworks <crayzhou@gmail.com>
2021-12-11 18:31:50 +0800

    The solutions for problem #1325, #1609

--- src/main/java/org/sean/tree/EvenOddTree.java
+++ src/main/java/org/sean/tree/EvenOddTree.java
@@ -1,2 +1,47 @@
-package org.sean.tree;public class EvenOddTree {
+package org.sean.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/***
+ * 1609. Even Odd Tree
+ */
+public class EvenOddTree {
+    public boolean isEvenOddTree(TreeNode root) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val % 2 == 1;
+
+        int level = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int cnt = queue.size();
+
+            boolean ascent = level % 2 == 0;
+            int last = -1;
+            for (int i = 0; i < cnt; i++) {
+                TreeNode node = queue.poll();
+                // System.out.print(" " + node.val);
+
+                if (node != null) {
+                    if (ascent) {
+                        if (last > 0 && last >= node.val || node.val % 2 == 0) return false;
+                        last = node.val;
+                    } else {
+                        if (last > 0 && last <= node.val || node.val % 2 == 1) return false;
+                        last = node.val;
+                    }
+
+                    if (node.left != null) queue.add(node.left);
+                    if (node.right != null) queue.add(node.right);
+                }
+            }
+            ++level;
+
+            // System.out.println("");
+        }
+
+        return true;
+    }
 }
--- src/main/java/org/sean/tree/NodeCleaner.java
+++ src/main/java/org/sean/tree/NodeCleaner.java
@@ -1,2 +1,19 @@
-package org.sean.tree;public class NodeCleaner {
+package org.sean.tree;
+
+/***
+ * 1325. Delete Leaves With a Given Value
+ */
+public class NodeCleaner {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) return null;
+
+        if (root.left == null && root.right == null && root.val == target) return null;
+
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if (root.left == null && root.right == null && root.val == target) root = null;
+
+        return root;
+    }
 }
